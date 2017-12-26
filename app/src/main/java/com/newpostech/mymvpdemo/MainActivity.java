ad89f2561f7e64d7bab1415a762c3c26e9689ce3
Thomasguoxp <Thomasguoxp@users.noreply.github.com>
2017-12-26 15:25:51 +0800

    Initial commit

---
+++ app/src/main/java/com/newpostech/mymvpdemo/MainActivity.java
@@ -0,0 +1,14 @@
+package com.newpostech.mymvpdemo;
+
+import android.os.Bundle;
+
+import com.jeremyfeinstein.slidingmenu.lib.app.SlidingFragmentActivity;
+
+public class MainActivity extends SlidingFragmentActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
